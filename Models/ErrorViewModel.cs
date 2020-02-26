19e09d38ccd6b1b4d3ef79a9ef538ebbf47fac9b
Billy <billymcfly@gmail.com>
2020-02-26 12:35:08 -0700

    Add default VS project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AccountsCodingChallenge.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

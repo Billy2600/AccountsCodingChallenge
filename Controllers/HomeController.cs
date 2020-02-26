3352852af38253beaf15aacbcb6d4aa6d4e3b1c4
Billy <billymcfly@gmail.com>
2020-02-26 13:15:13 -0700

    Add AccountService.GetAccountsAsync(), build out its logic, and use it in HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,10 +12,12 @@ namespace AccountsCodingChallenge.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private IAccountService _accountService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IAccountService accountService)
         {
             _logger = logger;
+            _accountService = accountService;
         }
 
         public IActionResult Index()
@@ -26,6 +28,17 @@ namespace AccountsCodingChallenge.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult AccountStatuses()
+        {
+            var accounts = _accountService.GetAccountsAsync().Result;
+            if(accounts.Count == 0)
+            {
+                return Error();
+            }
+
+            return View(accounts);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Models/AccountService.cs
+++ Models/AccountService.cs
@@ -2,11 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using AccountsCodingChallenge.Models;
+using Newtonsoft.Json;
 
 namespace AccountsCodingChallenge.Models
 {
-    public class AccountService
+    public class AccountService : IAccountService
     {
-         
+        private const string accountApiUrl = "https://frontiercodingtests.azurewebsites.net/api/accounts/getall";
+
+        public async Task<List<Account>> GetAccountsAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var accountList = new List<Account>();
+                    using(var httpClient = new HttpClient())
+                    {
+                        using (var response = await httpClient.GetAsync(accountApiUrl))
+                        {
+                            string responseJson = await response.Content.ReadAsStringAsync();
+                            accountList = JsonConvert.DeserializeObject<List<Account>>(responseJson);
+                            return accountList;
+                        }
+                    }
+                }
+                catch (HttpRequestException httpReqException)
+                {
+                    Console.WriteLine(httpReqException.Message); // TODO: In a 'real' project, this would probably get logged somewhere with more info
+                    return new List<Account>();
+                }
+            }
+        }
     }
 }
